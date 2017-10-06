ab74f85e9e08d73693490332fd57750818f3ac22
Pete Ashworth <petesashworth@gmail.com>
2017-10-06 17:27:56 -0600

    update react eslint rules

--- configurations/react.js
+++ configurations/react.js
@@ -9,10 +9,13 @@ module.exports = {
     'jsx-a11y',
   ],
   rules: {
+    'generator-star-spacing': 'off',
     'react/jsx-filename-extension': 'off',
     'react/jsx-max-props-per-line': 'error',
     'react/jsx-closing-bracket-location': ['error', 'props-aligned'],
-    'react/no-unused-prop-types': ['error', { skipShapeProps: true }],
-    'import/named': 'error'
+    'react/no-unused-prop-types': ['error', { 'skipShapeProps': true,  }],
+    'react/prefer-stateless-function': ['error', { 'ignorePureComponents': true,  }],
+    'import/named': 'error',
+    'no-param-reassign': ['error', {'props': true, 'ignorePropertyModificationsFor': ['req'] }],
   },
 };
