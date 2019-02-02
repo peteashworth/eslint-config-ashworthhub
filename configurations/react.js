af214f7ba5e9751479e3bee2dd4a83e520c5b5ef
Pete Ashworth <petesashworth@gmail.com>
2019-02-02 13:24:37 -0700

    set react/destructuring-assignment to warn

--- configurations/react.js
+++ configurations/react.js
@@ -13,9 +13,23 @@ module.exports = {
     'react/jsx-filename-extension': 'off',
     'react/jsx-max-props-per-line': 'error',
     'react/jsx-closing-bracket-location': ['error', 'props-aligned'],
-    'react/no-unused-prop-types': ['error', { 'skipShapeProps': true,  }],
-    'react/prefer-stateless-function': ['error', { 'ignorePureComponents': true,  }],
+    'react/no-unused-prop-types': ['error', {
+      skipShapeProps: true,
+    }],
+    'react/prefer-stateless-function': ['error', {
+      ignorePureComponents: true,
+    }],
     'import/named': 'error',
-    'no-param-reassign': ['error', {'props': true, 'ignorePropertyModificationsFor': ['req'] }],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: ['req'],
+    }],
+    'react/destructuring-assignment': [
+      'warn',
+      'always',
+      {
+        ignoreClassFields: true,
+      },
+    ],
   },
 };
